febc13b8ea739a6cdb6aa7a8f81edf367751b8da
Cedric M <zelogik+github@gmail.com>
2022-02-05 11:27:42 +0100

    Update RicinoNext-controller.ino
    Remove unused function, remove typedef, add comment

--- Software/Controller/RicinoNext-controller.ino
+++ Software/Controller/RicinoNext-controller.ino
@@ -125,7 +125,7 @@ UI_config uiConfig;
 //  Main Data struct
 // One Struct to keep every player data + sorted at each loop.
 // ----------------------------------------------------------------------------
-typedef struct {
+struct ID_Data_sorted{
   public:
     // get info from i2c/gate
     uint32_t ID;
@@ -288,20 +288,20 @@ typedef struct {
         //  0s   4s   7s       10s  13s  16s
         //  |    |    |    ->   |    |    |
     }
-} ID_Data_sorted;
+};
 
 ID_Data_sorted idData[NUMBER_RACER + 1]; // number + 1, [0] is the tmp for rank change, and so 1st is [1] and not [0] and so on...
 
 
 // ----------------------------------------------------------------------------
-//  Buffer Struct: Sort-Of simple buffer for i2c request
+//  Buffer Struct: Sort-Of simple buffer for i2c request from gates
 // ----------------------------------------------------------------------------
-typedef struct {
+struct ID_buffer{
     uint32_t ID = 0;
     uint8_t gateNumber = 0;
     uint32_t totalLapsTime = 0; // in millis ?
     bool isNew = false; //
-} ID_buffer;
+};
 
 ID_buffer idBuffer[NUMBER_RACER];
 
@@ -315,7 +315,7 @@ enum race_state_t {
     START,         // Run the warm-up phase, (light, beep,etc) (no registering player for the moment)
     RACE,          // enable all gate receiver and so, update Race state (send JSON, sendSerial) etc...
     FINISH,        // 1st player have win, terminate after 20s OR all players have finished.
-    STOP           // finished auto/manual 
+    STOP           // finished auto/manual goto WAIT
 };
 
 race_state_t raceState = RESET;
@@ -500,6 +500,7 @@ class Race {
 
 Race race = Race();
 
+
 // ----------------------------------------------------------------------------
 //  Web Stuff: Error function config JSON
 // is used somewhere? don't remember!
@@ -508,26 +509,12 @@ void notFound(AsyncWebServerRequest* request) {
   request->send(404, "text/plain", "Not found");
 }
 
-// ----------------------------------------------------------------------------
-//  Web Stuff: Used at new connection or broadcast confToJSON
-// todo: remove as confToJson make everything...
-// ----------------------------------------------------------------------------
-void notifyClients() {
-    StaticJsonDocument<JSON_BUFFER> json;
-    // json["status"] = led.on ? "on" : "off";
-
-    char buffer[JSON_BUFFER];
-    // size_t len = serializeJson(json, buffer);
-    // ws.textAll(buffer, len);
-}
-
 
 // ----------------------------------------------------------------------------
 //  Web Stuff: struct --> json<char[size]>
 //  char test[512];
 //  confToJSON(&uiConfig, test);
-//   Serial.println(test);
-// todo: need a special function to calculate the proper and dynamic size
+// todo: need a special function to calculate dynamic size
 // ----------------------------------------------------------------------------
 void confToJSON(char* output){ // const struct UI_config* data,
   StaticJsonDocument<JSON_BUFFER_CONF> doc;
@@ -572,6 +559,7 @@ void JSONToConf(const char* input){ // struct UI_config* data,
 
   const char *obj_p = obj["state"];
 
+  // todo: make a JsonObject loop.
   if ( obj_p != nullptr)
   {
       const bool stt = (char)atoi(obj_p);
@@ -582,6 +570,8 @@ void JSONToConf(const char* input){ // struct UI_config* data,
   if (obj.containsKey("laps"))
   {
       uiConfig.laps = doc["conf"]["laps"];
+              Serial.println(uiConfig.laps);
+
   }
   if (obj.containsKey("players"))
   {
@@ -599,6 +589,7 @@ void JSONToConf(const char* input){ // struct UI_config* data,
   {
       uiConfig.light_brightness = doc["conf"]["light_brightness"];
   }
+  // todo: need to find ID/position
   if (obj.containsKey("names"))
   {
       uint8_t count = 0;
@@ -649,6 +640,7 @@ void onEvent(AsyncWebSocket       *server,
             if (info->final && (info->index == 0) && (info->len == length)) {
                 if (info->opcode == WS_TEXT)
                 {
+                    // todo: Add an UI lock/unlock state?
                     JSONToConf((char*)data);
 
                     confToJSON(json);
@@ -990,6 +982,7 @@ void WriteJSONLive(uint32_t ms, uint8_t protocol){
 
   // Send "live" JSON section
   // todo: maybe add a millis delay to don't DDOS client :-D
+  // todo: optimization, send only new value! (so need more memory to store lastValue)
   for (uint8_t i = 1; i < (NUMBER_RACER + 1) ; i++){
     if (idData[i].positionChange[protocol] == true || idData[i].needGateUpdate(protocol))
     {
